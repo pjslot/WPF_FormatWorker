3497289a09bc029eabe3192bb3dd387bab190b0a
pjslot <pjslot@gmail.com>
2022-11-20 13:25:27 +0300

    1st git commit

---
+++ WPF_FormatWorker/ChartWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WPF_FormatWorker
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            // Создаем область построения ChartArea
+            chart.ChartAreas.Add(new ChartArea("defaultArea"));
+            // Добавляем ряд данных "defaultArea"
+            chart.Series.Add(new Series("Series1"));
+            chart.Series["Series1"].ChartArea = "defaultArea";
+            chart.Series["Series1"].ChartType = SeriesChartType.Pie;
+            // добавляем данные
+            int[] axisXData = new int[] { 1, 2, 3, 4, 5 };
+            int[] axisYData = new int[] { 56, 34, 15, 11, 5 };
+            chart.Series["Series1"].Points.DataBindXY(axisXData, axisYData);
+
+
+        }
+
+
+    }
+}
---
+++ WPF_FormatWorker/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using GenericParsing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Forms.DataVisualization;
+using System.Data;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+
+namespace WPF_FormatWorker
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //симуляция нажатия кнопки - на время отладки
+            Button_Click_LoadCSV(but1, null);
+        }
+
+        //кнопка ОТКРЫТЬ CSV
+        private void Button_Click_LoadCSV(object sender, RoutedEventArgs e)
+        {
+            //объект диалога открытия файла
+            var f = new OpenFileDialog();
+            //фильтр диалога открытия файла
+            f.Filter = "Файлы CSV| *.CSV";
+            //переменная имени файла
+            string file = "";
+
+
+            //вызов диалога открытия файла
+            //ЗАКРЫТО НА ОТЛАДКУ if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK) file = f.FileName;
+            
+            //выгрузка данных на форму
+            file = @"C:\Users\VS\Downloads\TOP500_202011.csv"; //убрать на релизе!
+            if (file != "")
+            {
+            using (GenericParserAdapter parser = new GenericParserAdapter(file))
+              {
+                // Разделитель CSV
+                parser.ColumnDelimiter = ';';
+                // Первая строка - заголовок
+                parser.FirstRowHasHeader = true;
+                // парсинг CSV в объект таблицы
+                DataTable dtComputers = parser.GetDataTable();
+                // выгрузка объекта таблицы в датагрид
+                dataGrid.ItemsSource = dtComputers.DefaultView;
+              }
+            }
+            
+
+        }
+
+        private void Button_Click_CountryDiag(object sender, RoutedEventArgs e)
+        {
+
+            Window1 ChartViewer = new Window1();
+            ChartViewer.Show();
+
+            
+
+        }
+    }
+}
