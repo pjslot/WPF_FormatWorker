eece5f9dc5757b2e1a640325c0ec306090fdf0df
pjslot <pjslot@gmail.com>
2022-11-22 00:20:58 +0300

    XML export added

--- WPF_FormatWorker/MainWindow.xaml.cs
+++ WPF_FormatWorker/MainWindow.xaml.cs
@@ -5,8 +5,9 @@ using System.Windows.Forms.Integration;
 using System.Windows.Forms.DataVisualization;
 using System.Data;
 using System.Windows.Forms.DataVisualization.Charting;
-
-
+using System.IO;
+using System.Windows.Controls;
+using System.Runtime.InteropServices;
 
 namespace WPF_FormatWorker
 {
@@ -19,6 +20,7 @@ namespace WPF_FormatWorker
         //публички для диаграммы
         public static int cores, power;
         public static string compName;
+        public static DataTable dtCompPublic;  
 
         public MainWindow()
         {
@@ -30,6 +32,8 @@ namespace WPF_FormatWorker
             cores = 0;
             power = 0;
             compName = "";
+         //   sliderText.Text = slider.Value.ToString();
+           
         }
 
         //КНОПКА ОТКРЫТЬ CSV
@@ -58,6 +62,8 @@ namespace WPF_FormatWorker
                 parser.FirstRowHasHeader = true;
                 // парсинг CSV в объект таблицы
                 DataTable dtComputers = parser.GetDataTable();
+                //выкидываем копию наружу что б другие могли работать
+                dtCompPublic = dtComputers;
                 // выгрузка объекта таблицы в датагрид
                 dataGrid.ItemsSource = dtComputers.DefaultView;
                 }            
@@ -86,6 +92,8 @@ namespace WPF_FormatWorker
             compName = row["Name"].ToString();
         }
 
+      
+
         //КНОПКА ОТРИСОВКИ ДИАГРАММЫ
         private void Button_Click_CountryDiag(object sender, RoutedEventArgs e)
         {
@@ -105,6 +113,54 @@ namespace WPF_FormatWorker
    
         }
 
-       
+        //реакция на прокрутку слайдера подрезки экспорта
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {    
+            int sliderCount = (int)slider.Value;
+            //прокидываю биндинг текстблока через резервное поле кнопки, напрямую почему то не получается, а биндинг слайдера через xaml даёт некрасивые double
+            but1.Uid=sliderCount.ToString();    
+           // sliderText.Text = "111";
+        }
+
+        //КНОПКА ВЫГРУЗКИ В XML
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //если табличка подгружена - выгружаем XML
+            if (dtCompPublic != null)
+            {    
+                //подрезка таблицы перед экспортом
+                int cutStart = (int)slider.Value;
+                int cutStop = dtCompPublic.Rows.Count;
+                int i = cutStart;              
+                 while (dtCompPublic.Rows.Count > cutStart)
+                {
+                    DataRow dr = dtCompPublic.Rows[i];
+                    dr.Delete();                
+                }
+
+                //выгружаем в XML
+                dtCompPublic.TableName = "Supercomputers Top500";
+                using (StreamWriter sw = new StreamWriter("data - XML EXPORT.xml"))
+                {
+                    dtCompPublic.WriteXml(sw);
+                }
+                MessageBoxResult result = System.Windows.MessageBox.Show("Файл XML экспортирован в корневую папку.",
+                                         "Успешно!",
+                                         MessageBoxButton.OK,
+                                         MessageBoxImage.Information);
+            }
+            else
+            //если таблички нет то предлагаем подгрузить
+            {
+                MessageBoxResult result = System.Windows.MessageBox.Show("Вначале подгрузите CSV таблицу!",
+                                          "CSV Таблица не подгружена",
+                                          MessageBoxButton.OK,
+                                          MessageBoxImage.Information);
+            }
+        }
+
     }
 }
+
+
+//СДЕЛАТЬ КНОПКУ ОТКАТА ОБРУБКА ТАБЛИЦЫ!
